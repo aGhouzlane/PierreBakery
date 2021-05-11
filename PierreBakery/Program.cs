c18b26e348bf508f32e7faee3e3619a602888595
aGhouzlane <ahmedghouzlane@gmail.com>
2021-05-11 00:08:01 -0400

    Add error handling

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -15,52 +15,66 @@ namespace PierreBakery
       int userInput;
       int breadCounter = 0;
       int pastryCounter = 0;
-      do
+      try
       {
-        Console.WriteLine("Welcome to Pierre's Bakery");
-        Console.WriteLine("Single Loaf of Bread: $5 - Buy Two Get One Free.");
-        Console.WriteLine("Single Pastry: $2 - Buy Three for $5.");
-        Console.WriteLine("Enter 1 to add a bread order.");
-        Console.WriteLine("Enter 2 to add a pastry order.");
-        Console.WriteLine("Enter 3 to check the price.");
-        Console.WriteLine("Enter 4 to checkout.");
-        userInput = int.Parse(Console.ReadLine());
-        int totalOrderPrice = 0;
-        switch (userInput)
+        do
         {
-          case 1:
-            int breadInput = int.Parse(Console.ReadLine());
-            Bread bread = new Bread(breadInput);
-            breadCounter = breadCounter + breadInput;
-            priceList.Add(bread.FindBreadOrderPrice());
-            break;
-          case 2:
-            int pastryInput = int.Parse(Console.ReadLine());
-            Pastry pastry = new Pastry(pastryInput);
-            pastryCounter = pastryCounter + pastryInput;
-            priceList.Add(pastry.FindPastryOrderPrice());
-            break;
-          case 3:
-            foreach (int item in priceList)
-            {
-              totalOrderPrice = totalOrderPrice + item;
-            }
-            Console.WriteLine("Total Order Price: $" + totalOrderPrice);
-            break;
-          case 4:
-            Bread newBread = new Bread(breadCounter);
-            Console.WriteLine("Thank you for choosing Pierre's Bakery!");
-            foreach (int item in priceList)
-            {
-              totalOrderPrice = totalOrderPrice + item;
-            }
-            Console.WriteLine("Total Order Price: $" + totalOrderPrice);
-            Console.WriteLine("Bread Loaves Quantity: " + breadCounter +
-            " + " + newBread.GetFreeBread() + " Free Bread loaf(ves)");
-            Console.WriteLine("Pastry Loaves Quantity: " + pastryCounter);
-            break;
-        }
-      } while (userInput != 4);
+          Console.WriteLine("Welcome to Pierre's Bakery");
+          Console.WriteLine("Single Loaf of Bread: $5 - Buy Two Get One Free.");
+          Console.WriteLine("Single Pastry: $2 - Buy Three for $5.");
+          Console.WriteLine("Enter 1 to add a bread order.");
+          Console.WriteLine("Enter 2 to add a pastry order.");
+          Console.WriteLine("Enter 3 to check the price.");
+          Console.WriteLine("Enter 4 to checkout.");
+
+          userInput = int.Parse(Console.ReadLine());
+          int totalOrderPrice = 0;
+          switch (userInput)
+          {
+            case 1:
+              int breadInput = int.Parse(Console.ReadLine());
+              Bread bread = new Bread(breadInput);
+              breadCounter = breadCounter + breadInput;
+              priceList.Add(bread.FindBreadOrderPrice());
+              break;
+            case 2:
+              int pastryInput = int.Parse(Console.ReadLine());
+              Pastry pastry = new Pastry(pastryInput);
+              pastryCounter = pastryCounter + pastryInput;
+              priceList.Add(pastry.FindPastryOrderPrice());
+              break;
+            case 3:
+              foreach (int item in priceList)
+              {
+                totalOrderPrice = totalOrderPrice + item;
+              }
+              Console.WriteLine("Total Order Price: $" + totalOrderPrice);
+              break;
+            case 4:
+              Bread newBread = new Bread(breadCounter);
+              Pastry newPastry = new Pastry(pastryCounter);
+              int temp = newPastry.GetDiscountPrice();
+              Console.WriteLine("Thank you for choosing Pierre's Bakery!");
+              foreach (int item in priceList)
+              {
+                totalOrderPrice = totalOrderPrice + item;
+              }
+              Console.WriteLine("Bread Loaves Quantity: " + breadCounter +
+              " + (" + newBread.GetFreeBread() + " Free Bread loaf(ves))");
+              Console.WriteLine("Pastry Quantity: " + pastryCounter);
+              Console.WriteLine("Total Order Price: $" + (totalOrderPrice - temp) + " ($"
+              + temp + " Discount was applied to the Total Price)");
+              break;
+            default:
+              Console.WriteLine("Input was not one of the menu entries.");
+              break;
+          }
+        } while (userInput != 4);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
     }
   }
 }
